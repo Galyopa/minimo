ccec2c620e4bae9294172715e99b23ebab9bc1b3
Denys Halopa <galyopa@gmail.com>
2022-03-14 21:36:17 +0200

    add HomePage

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
-  
+  $('.banner').slick({
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 3000,
+  });
+
+  $('.nav__btn').on('click', function () {
+    $('.nav__list').toggleClass('active');
+    $(this).toggleClass('active');
+  });
+
+
 });
